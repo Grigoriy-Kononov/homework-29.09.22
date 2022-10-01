ebc3cd0c4fadce51a1fdb29f0baf027bc7f027f0
Grigory <kononoff88@gmail.com>
2022-10-01 06:58:44 +0300

    homework run

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] filledArray = new int[size];
+
+    for (int i = 0; i < filledArray.Length; i++)
+    {
+        filledArray[i] = new Random().Next(min, max + 1);
+    }
+    return filledArray;
+}
